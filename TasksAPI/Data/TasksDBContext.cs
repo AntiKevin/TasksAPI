9e3eb25cd1504afa93a0ab2420dafcb7beb08bc6
kevin3467 <kevin_3467@yahoo.com>
2023-06-08 15:41:31 -0300

    Adicionar arquivos de projeto.

---
+++ TasksAPI/Data/TasksDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasksAPI.Models;
+
+namespace TasksAPI.Data
+{
+    public class TasksDBContext: DbContext
+    {
+        public TasksDBContext(DbContextOptions<TasksDBContext> options)
+            : base(options)
+        { 
+        }
+        
+        public DbSet<UsuarioModel> Usuarios { get; set;}
+        public DbSet<TarefasModel> Tarefas { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TasksAPI/Models/TarefasModel.cs
@@ -0,0 +1,13 @@
+﻿using TasksAPI.Enums;
+
+namespace TasksAPI.Models
+{
+    public class TarefasModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public StatusTarefa Status { get; set; }
+
+    }
+}
